5f9412ef4efa4568d5fee5048f284f8da2059ca0
sparkrico <sparkrico@yahoo.com.cn>
2013-06-19 14:11:12 +0800

    add layout-large-land dual pane

--- v2ex/src/com/sparkrico/v2ex/NodeMenuFragment.java
+++ v2ex/src/com/sparkrico/v2ex/NodeMenuFragment.java
@@ -13,7 +13,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
